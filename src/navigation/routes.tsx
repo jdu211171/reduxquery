3adba97c578e01b012a16a2a9049310ed4cd749b
jdu211171 <211171m@jdu.uz>
2024-03-25 10:44:02 +0500

    build configured

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from '../views/Home.tsx';
 import {NavigationContainer} from '@react-navigation/native';
+import WelcomeScreen from '../views/Welcome.tsx';
 
 const Stack = createNativeStackNavigator();
 
 function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        {/*<Stack.Screen name="Home" component={HomeScreen} />*/}
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
